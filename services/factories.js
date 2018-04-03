0429a71f0614eb5591b2e3f46f514f6425ea105d
Salvador Romero <salva.romero.aguilar@gmail.com>
2018-04-03 23:55:58 +0200

    Owners and factory update v0.1.0

--- services/factories.js
+++ services/factories.js
@@ -1,25 +1,31 @@
 APP.service('defaultFactory', function($http) {
     return {
+        // Get the list of all restaurants
         getRestaurants: function() 
         {
             return $http.get('http://localhost/Restaurante/restaurant_api/owners'); 
         },
+        // Get the list of all owners
         getOwners: function ()
         {
             return $http.get('http://localhost/Restaurante/restaurant_api/owners'); 
         },
+        // Get a owner giving id
         getOwner: function (id_owner)
         {
             return $http.get("http://localhost/Restaurante/restaurant_api/owners/"+id_owner);
         },
+        // Get the restaurants giving id
         getRestaurantById: function ()
         {
             return $http.get('http://localhost/Restaurante/restaurant_api/owners'); 
         },
+        // Save a new owner, need the object of new owner data
         saveOwner: function(obj)
         {
             return $http.post('http://localhost/Restaurante/restaurant_api/owners', obj);
         },
+        // Remove the owner giving id
         deleteOwner: function(id_owner)
         {
             return $http.delete('http://localhost/Restaurante/restaurant_api/owners/'+id_owner);
